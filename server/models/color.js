0a019b0fbd236da9b6e0276c74064860e306bf16
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-12 17:03:16 +0900

    define models and relations

---
+++ server/models/color.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define("color", {
+    name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
+  });
