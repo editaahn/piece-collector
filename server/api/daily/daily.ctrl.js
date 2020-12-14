7bde0eaa73654e040ca4b1356ab7360c4fad9585
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-14 16:48:56 +0900

    refactor create function
    then -> async/await

--- server/api/daily/daily.ctrl.js
+++ server/api/daily/daily.ctrl.js
@@ -21,43 +21,35 @@ const show = async (req, res) => {
 };
 
 const create = async (req, res) => {
-  const { date, title, article, songs, colorId } = req.body;
+  const { date, songs } = req.body;
   if (!date) return res.status(400).end();
 
-  Diary.create(
-    {
-      date,
-      title,
-      article,
-      colorId,
-      songs: songs.filter((song) => !song.hasOwnProperty("id")),
-    },
-    { include: [Song, Color] }
-  )
-    .then(async (diary) => {
-      await diary.addSongs(
-        songs.reduce((list, song) => {
-          return song.hasOwnProperty("id") ? list.concat(song.id) : list;
-        }, [])
-      );
-      return await diary.id;
-    })
-    .then((id) => {
-      return Diary.findOne({
-        where: { id },
-        include: [Song],
-      });
-    })
-    .then((diary) => {
-      res.status(201).json(diary);
-    })
-    .catch((err) => {
-      const DUPLICATION_ERROR_NUMBER = 1062;
-      if (err.parent.errno === DUPLICATION_ERROR_NUMBER) {
-        return res.status(409).end();
-      }
-      res.status(500).end();
+  try {
+    const diary = await Diary.create(
+      {
+        ...req.body,
+        songs: songs.filter((song) => !song.hasOwnProperty("id")),
+      },
+      { include: [Song, Color] }
+    );
+    await diary.addSongs(
+      songs.reduce((list, song) => {
+        return song.hasOwnProperty("id") 
+          ? list.concat(song.id) : list;
+      }, [])
+    );
+    const result = await Diary.findOne({
+      where: { id: diary.id },
+      include: [Song],
     });
+    res.status(201).json(result);
+  } catch(err) {
+    const ERROR_NUMBER = 1062;
+    if (err.parent.errno === ERROR_NUMBER) {
+      return res.status(409).end();
+    }
+    res.status(500).end();
+  };
 };
 
 module.exports = { show, create };
