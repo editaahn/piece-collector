925725c8c6fdd7cbca67ec9052b9a33a3e76e7db
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-16 17:35:17 +0900

    add module 'cors' on server package list

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const session = require("express-session");
 const monthlyRouter = require("./api/monthly");
 const dailyRouter = require("./api/daily");
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV !== "test") {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // routers
 app.use("/monthly", monthlyRouter);
