fb290e26650f66abf71cd004e44e3abd5d8aa341
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-20 13:38:39 +0900

    routing daily

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,7 @@
 import "./main.css";
 import Toast from "./views/components/Toast";
 import { onNavigate, initialRoutes, routes } from "./router.js";
+import { parsePath } from "./libraries/parsePath";
 
 class App {
   constructor() {
@@ -13,3 +14,8 @@ class App {
 document.addEventListener("DOMContentLoaded", () => {
   new App();
 });
+
+window.onpopstate = () => {
+  const parsedPath = parsePath(window.location.pathname);
+  return routes[parsedPath].render();
+};
--- client/src/views/pages/Daily.js
+++ client/src/views/pages/Daily.js
@@ -1,16 +1,30 @@
 import Component from "../../state-management/Component.js";
 import store from "../../state-management/index.js";
+import { apiBaseUrl } from "../../libraries/constants.js";
+import { getIdParameter } from "../../libraries/parsePath.js";
 const axios = require("axios");
 
 export default class Daily extends Component {
   constructor({ $root }) {
     super({
-      store
+      store,
     });
     this.$root = $root;
   }
+
+  async getDailyData(id) {
+    const result = await axios.get(
+      `${apiBaseUrl}/daily/${id}`
+    );
+    this.diaries = result.data;
+  }
+
   render() {
-    this.$root.innerHTML = "";
-    this.$root.textContent = "It's a diary"
+    this.diaryId = getIdParameter();
+
+    this.getDailyData(this.diaryId).then(() => {
+      this.$root.innerHTML = "";
+      this.$root.textContent = "It's a diary";
+    });
   }
 }
