0d88fcd1cfb61a96b9bddda33c2f0f4729f00b3b
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-24 08:24:04 +0900

    submit diary

--- client/src/views/Daily/components/DiaryHeader.js
+++ client/src/views/Daily/components/DiaryHeader.js
@@ -1,11 +1,16 @@
+import song from "../../../../../server/models/song";
 import goBack from "../../../images/goBack.svg";
+import { apiBaseUrl } from "../../../libraries/constants.js";
+const axios = require("axios");
 
 export default class DiaryHeader {
-  constructor({ $nav, data: { date } }) {
+  constructor({ $nav, data: { type, date } }) {
     this.$nav = $nav;
+    this.type = type;
     this.date = date;
 
     this.render();
+    this.$submit?.addEventListener("click", this.submit.bind(this));
   }
 
   render() {
@@ -17,12 +22,61 @@ export default class DiaryHeader {
     this.$goBack.className = "Diary__goBack";
     this.$goBack.innerHTML = `<img src=${goBack} alt="goBack"/>`;
 
-    this.$submit = document.createElement("button");
-    this.$submit.className = "Diary__submit";
-    this.$submit.innerHTML = "done";
+    this.$submitDiv = document.createElement("div");
+    this.$submitDiv.className = "Diary__submitDiv";
+    if (this.type === "new") {
+      this.$submit = document.createElement("button");
+      this.$submit.className = "Diary__submit";
+      this.$submit.innerHTML = "done";
+      this.$submitDiv.appendChild(this.$submit);
+    }
 
     this.$nav.appendChild(this.$goBack);
     this.$nav.appendChild(this.$date);
-    this.$nav.appendChild(this.$submit);
+    this.$nav.appendChild(this.$submitDiv);
+  }
+
+  async create(data) {
+    try {
+      const result = await axios.post(`${apiBaseUrl}/daily`, data);
+      if (result.status === 201) {
+        return { status: 201, message: "등록이 완료되었습니다." };
+      }
+      throw Error(result.status);
+    } catch (err) {
+      // 추후 에러 처리
+      console.error(err);
+      return { status: err, message: "등록에 실패했습니다." };
+    }
+  }
+
+  submit() {
+    const data = {
+      date: new Date(this.date),
+      title: document.querySelector(".Diary__title")?.textContent,
+      colorId: document.querySelector(".Diary__color--selected")?.dataset
+        .colorid,
+      songs: [],
+      article: document.querySelector(".Diary__article")?.innerHTML,
+    };
+
+    // 등록되면 done 버튼 비노출 및 등록완료 알럿
+    const isValid = this.validate(data);
+    if (isValid) {
+      this.create(data).then((response) => {
+        alert(response.message); // 토스트팝업으로 변경 필요
+        this.$submit?.remove();
+      });
+    } else {
+      alert("내용을 1개 입력해주세요.");
+    }
+  }
+
+  validate(data) {
+    // title, song, article 셋중 하나 있어야함
+    if (data.title?.length || data.song?.length || data.article?.length) {
+      return true;
+    }
+    return false;
   }
 }
--- client/src/views/Daily/page/Daily.js
+++ client/src/views/Daily/page/Daily.js
@@ -4,10 +4,8 @@ import DiaryTitle from "../components/DiaryTitle.js";
 import DiarySong from "../components/DiarySong.js";
 import DiaryArticle from "../components/DiaryArticle.js";
 import DiaryColor from "../components/DiaryColor.js";
-import { setDocumentTheme } from "../../../libraries/themeColor";
 import { apiBaseUrl } from "../../../libraries/constants.js";
 import { getParameter } from "../../../libraries/parsePath.js";
-import DiaryHeader from "../components/DiaryHeader.js";
 const axios = require("axios");
 
 export default class Daily extends Component {
@@ -29,20 +27,8 @@ export default class Daily extends Component {
     parameter !== "/new" && (await this.getDailyData(parameter));
 
     this.$root.innerHTML = "";
-
-    const $nav = document.createElement("header");
     const $page = document.createElement("section");
-
-    $nav.className = "Diary__header";
-    $page.className = "page Daily";
-
-    this.header = new DiaryHeader({
-      $nav,
-      data: {
-        date:
-          this.diary?.date ?? store.state.newDairyDate,
-      },
-    });
+    $page.className = "Daily";
 
     this.title = new DiaryTitle({
       $page,
@@ -65,7 +51,6 @@ export default class Daily extends Component {
       data: {
         id: this.diary?.id ?? "",
         songs: this.diary?.songs ?? [],
-        color: this.diary?.color ?? "",
       },
     });
 
@@ -77,9 +62,6 @@ export default class Daily extends Component {
       },
     });
 
-    setDocumentTheme(this.diary?.color.id);
-
-    this.$root.appendChild($nav);
     this.$root.appendChild($page);
   }
 }
